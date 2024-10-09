e2445249d849c7aaed7d8f02dda39279f41649ce
Sam Randa <samrandaofficial@gmail.com>
2024-10-08 20:20:42 -0400

    Added sanity checks for login and register pages

--- frontend/tests/basicPages.spec.ts
+++ frontend/tests/basicPages.spec.ts
@@ -3,9 +3,24 @@ test.describe("Test homepage elements", async () => {
   test('Contains header text', async ({ page }) => {
     await page.goto('/');
   
-    // Expect a title "to contain" a substring
     const login = await page.getByText("music viz mqp!")
     await expect(login).toBeVisible();
   });
-})
+});
+
+test.describe("Test login/register elements", async () => {
+  test('Login contains welcome text', async ({ page }) => {
+    await page.goto('/login');
+  
+    const login = await page.getByText("welcome!");
+    await expect(login).toBeVisible();
+  });
+
+  test('Register contains welcome text', async ({ page }) => {
+    await page.goto('/register');
+  
+    const login = await page.getByText("welcome!");
+    await expect(login).toBeVisible();
+  });
+});
 
