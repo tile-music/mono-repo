0845eb761ae47f7aa28d92e1d361c0094025ac32
Sam Randa <samrandaofficial@gmail.com>
2024-10-08 20:21:14 -0400

    Added missing semicolons

--- frontend/tests/login.spec.ts
+++ frontend/tests/login.spec.ts
@@ -4,8 +4,8 @@ test.describe("Test login flow", async () => {
   test('User must enter email', async ({ page }) => {
     await page.goto('/login');
   
-    await page.getByPlaceholder("password").fill("testpassword")
-    await page.getByText("log in").click()
+    await page.getByPlaceholder("password").fill("testpassword");
+    await page.getByText("log in").click();
     
     await expect(page.getByText("please enter an email")).toBeVisible();
   });
@@ -13,19 +13,19 @@ test.describe("Test login flow", async () => {
   test('User must enter password', async ({ page }) => {
     await page.goto('/login');
   
-    await page.getByPlaceholder("email").fill("test@test.com")
-    await page.getByText("log in").click()
+    await page.getByPlaceholder("email").fill("test@test.com");
+    await page.getByText("log in").click();
     
     await expect(page.getByText("please enter a password")).toBeVisible();
   });
 
   test('Invalid credentials are rejected', async ({ page }) => {
     await page.goto('/login');
-  
-    await page.getByPlaceholder("email").fill("test@test.com")
-    await page.getByPlaceholder("password").fill("testpassword")
-    await page.getByText("log in").click()
+
+    await page.getByPlaceholder("email").fill("test@test.com");
+    await page.getByPlaceholder("password").fill("testpassword");
+    await page.getByText("log in").click();
     
     await expect(page.getByText("invalid credentials")).toBeVisible();
   });
-})
+});
