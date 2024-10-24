a85b4fcc662c340c6656a540c01f30d01b3fa543
Ivy Bixler <ivybixler@gmail.com>
2024-10-24 11:15:19 -0400

    grrrrrrrrr this is frustrating, ive messed around with a few packages but have not had luck geting tls/ssl working on my local machine

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
 	server: {
+		https: {
+			key: 'cert/ca-key.pem',
+			cert: 'cert/ca-cert.pem'
+		},
 		watch: {
 		  usePolling: true
 		}
