5f3dd9550b82af76fc2d701e21fb706db1628310
notsoli <samrandaofficial@gmail.com>
2024-10-25 13:37:18 -0400

    Implemented basic square display

--- frontend/src/routes/(app)/display/+page.svelte
+++ frontend/src/routes/(app)/display/+page.svelte
@@ -1,1 +1,41 @@
+<script lang="ts">
+    import Square from './Square.svelte';
+
+    let squares  = [
+        { x: 0.36, y: 0.38, size: 0.24 },
+        { x: 0.6, y: 0.36, size: 0.24 },
+        { x: 0.42, y: 0.2, size: 0.18 },
+        { x: 0.44, y: 0.62, size: 0.16 },
+        { x: 0.22, y: 0.4, size: 0.14 },
+        { x: 0.32, y: 0.62, size: 0.12 },
+        { x: 0.6, y: 0.6, size: 0.12 },
+        { x: 0.32, y: 0.28, size: 0.1 },
+        { x: 0.6, y: 0.26, size: 0.1 },
+        { x: 0.22, y: 0.54, size: 0.1 }
+    ];
+</script>
+
 <h1>Art Display</h1>
+<div id="container">
+    <div id="display">
+        {#each squares as square}
+            <Square square={square} />
+        {/each}
+    </div>
+</div>
+
+<style>
+    #container {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #display {
+        height: calc(100vh - 150px);
+        aspect-ratio: 1 / 1;
+        position: relative;
+    }
+</style>
---
+++ frontend/src/routes/(app)/display/Square.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    type squareInfo = {
+        x: number
+        y: number
+        size: number
+    };
+
+    export let square: squareInfo;
+
+    function percent(decimal: number) {
+        return 100*decimal + "%";
+    }
+
+    $: style = `
+        left: ${percent(square.x)};
+        top: ${percent(square.y)};
+        width: ${percent(square.size)};
+        height: ${percent(square.size)};
+    `
+</script>
+
+<div id="square" style={style}></div>
+
+<style>
+    #square {
+        z-index: 0;
+        position: absolute;
+        background-color: white;
+        border: 2px solid black;
+        outline: 1px solid black;
+        box-sizing: border-box;
+        transition: transform ease-in 0.2s, z-index linear 0.2s;
+    }
+
+    #square:hover {
+        z-index: 10;
+        transform: scale(1.3);
+        transition: transform ease-out 0.2s, z-index linear 0.2s;
+    }
+</style>
