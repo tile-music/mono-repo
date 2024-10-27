745478bd95f03650d912b325d95f94b978f78326
notsoli <samrandaofficial@gmail.com>
2024-10-27 19:37:42 -0400

    Replaced hardcoded squares with Ava's square packing algorithm

--- frontend/src/routes/(app)/display/+page.svelte
+++ frontend/src/routes/(app)/display/+page.svelte
@@ -1,25 +1,40 @@
 <script lang="ts">
     import Square from './Square.svelte';
+    import { rankSongs } from './display';
 
-    let squares  = [
-        { x: 0.36, y: 0.38, size: 0.24 },
-        { x: 0.6, y: 0.36, size: 0.24 },
-        { x: 0.42, y: 0.2, size: 0.18 },
-        { x: 0.44, y: 0.62, size: 0.16 },
-        { x: 0.22, y: 0.4, size: 0.14 },
-        { x: 0.32, y: 0.62, size: 0.12 },
-        { x: 0.6, y: 0.6, size: 0.12 },
-        { x: 0.32, y: 0.28, size: 0.1 },
-        { x: 0.6, y: 0.26, size: 0.1 },
-        { x: 0.22, y: 0.54, size: 0.1 }
-    ];
+    import { generateFullArrangement } from './pack';
+
+    import type { PageData } from './$types';
+    export let data: PageData;
+
+    const test = generateFullArrangement(1, 14, 18, 0.1, 0.2);
+
+    const squares: { x: number, y: number, size: number}[] = [];
+    for (const entry of test) {
+        squares.push({ x: entry.x, y: entry.y, size: entry.width })
+    }
+
+    // let squares  = [
+    //     { x: 0.36, y: 0.38, size: 0.24 },
+    //     { x: 0.6, y: 0.36, size: 0.24 },
+    //     { x: 0.42, y: 0.2, size: 0.18 },
+    //     { x: 0.44, y: 0.62, size: 0.16 },
+    //     { x: 0.22, y: 0.4, size: 0.14 },
+    //     { x: 0.32, y: 0.62, size: 0.12 },
+    //     { x: 0.6, y: 0.6, size: 0.12 },
+    //     { x: 0.32, y: 0.28, size: 0.1 },
+    //     { x: 0.6, y: 0.26, size: 0.1 },
+    //     { x: 0.22, y: 0.54, size: 0.1 }
+    // ];
+
+    const result = rankSongs(data.songs);
 </script>
 
 <h1>Art Display</h1>
 <div id="container">
     <div id="display">
-        {#each squares as square}
-            <Square square={square} />
+        {#each squares as square, i}
+            <Square square={square} song={result[i].song}/>
         {/each}
     </div>
 </div>
