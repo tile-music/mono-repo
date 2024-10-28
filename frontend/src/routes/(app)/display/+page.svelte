8f1731c16091bf3d78481e1c3e22eb645abd0447
notsoli <samrandaofficial@gmail.com>
2024-10-28 14:17:10 -0400

    Removed hardcoded square values and added comments

--- frontend/src/routes/(app)/display/+page.svelte
+++ frontend/src/routes/(app)/display/+page.svelte
@@ -7,26 +7,16 @@
     import type { PageData } from './$types';
     export let data: PageData;
 
-    const test = generateFullArrangement(1, 14, 18, 0.1, 0.2);
+    // generate 14-18 squares with a small range of offsets
+    const arrangement = generateFullArrangement(1, 14, 18, 0.1, 0.2);
 
+    // translate the output of arrangement into a form usable by the Square component
     const squares: { x: number, y: number, size: number}[] = [];
-    for (const entry of test) {
-        squares.push({ x: entry.x, y: entry.y, size: entry.width })
+    for (const square of arrangement) {
+        squares.push({ x: square.x, y: square.y, size: square.width })
     }
 
-    // let squares  = [
-    //     { x: 0.36, y: 0.38, size: 0.24 },
-    //     { x: 0.6, y: 0.36, size: 0.24 },
-    //     { x: 0.42, y: 0.2, size: 0.18 },
-    //     { x: 0.44, y: 0.62, size: 0.16 },
-    //     { x: 0.22, y: 0.4, size: 0.14 },
-    //     { x: 0.32, y: 0.62, size: 0.12 },
-    //     { x: 0.6, y: 0.6, size: 0.12 },
-    //     { x: 0.32, y: 0.28, size: 0.1 },
-    //     { x: 0.6, y: 0.26, size: 0.1 },
-    //     { x: 0.22, y: 0.54, size: 0.1 }
-    // ];
-
+    // sort listened songs by times listened
     const result = rankSongs(data.songs);
 </script>
 
