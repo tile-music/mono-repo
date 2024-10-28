5bccc0f95ad8a2c849ce3b3494e85c8c5b968ad6
notsoli <samrandaofficial@gmail.com>
2024-10-28 19:15:18 -0400

    Displayed additional listening information & revamped styling

--- frontend/src/routes/(app)/listening/+page.svelte
+++ frontend/src/routes/(app)/listening/+page.svelte
@@ -2,28 +2,74 @@
     import type { PageData } from './$types';
     export let data: PageData;
     import Song from './Song.svelte';
+
+    import { processSongs } from './processSongs';
+    const processedSongs = processSongs(data.songs);
 </script>
 
-<h1>Listening Data</h1>
-<div class="songs">
-    {#if data.songs != null}
-        {#each data.songs as song}
-            <Song {song} />
-        {/each}
-    {:else}
-        <p>No listening data yet!</p>
-    {/if}
+<div id="container">
+    <h1>Listening Data</h1>
+    <div id="headers">
+        <h2 id="art">art</h2>
+        <h2 id="title">title</h2>
+        <h2 id="artist">artist</h2>
+        <h2 id="album">album</h2>
+        <h2 id="duration">duration</h2>
+        <h2 id="plays">plays</h2>
+    </div>
+    <div id="songs">
+        {#if data.songs != null}
+            {#each processedSongs as song}
+                <Song {song} />
+            {/each}
+        {:else}
+            <p>No listening data yet!</p>
+        {/if}
+    </div>
 </div>
 
 <style>
-    h1 {
-        margin-bottom: 20px;
+    #container {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
     }
 
-    .songs {
+    h1 {
+        margin-bottom: 1em;
+    }
+
+    #headers {
+        display: flex;
+        padding: 1em 0 1em 40px;
+        gap: 10px;
+        position: sticky;
+        top: 0;
+        background-color: var(--background);
+    }
+
+    /* these values are manually synced with the Song component */
+    #art {
+        width: 50px;
+    }
+
+    #title, #album {
+        width: 300px;
+    }
+
+    #artist {
+        width: 200px;
+    }
+
+    #duration, #plays {
+        width: 100px;
+    }
+
+    #songs {
         display: flex;
         flex-direction: column;
         gap: 20px;
         align-items: flex-start;
+        min-height: 0;
     }
 </style>
--- frontend/src/routes/(app)/listening/Song.svelte
+++ frontend/src/routes/(app)/listening/Song.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
-    import type { SongInfo } from '../song';
-    export let song: SongInfo;
-    let album = song.albums[0];
+    import type { ProcessOutput } from './processSongs';
+    export let song: ProcessOutput;
+    $: album = song.albums[0];
+    $: duration = calculateDuration(song.duration);
+
+    function calculateDuration(ms: number) {
+        const minutes = Math.floor(ms / 60000);
+        const seconds = ((ms % 60000) / 1000);
+        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds.toFixed(0);
+    }
 </script>
 
 <div class="song">
-    <img src={album.image} alt={`The album art for ${album.title} by ${album.artists.join(', ')}.`}>
+    <p class="repetitions">{(song.repetitions > 1) ? "x" + song.repetitions : ""}</p>
+    <img class="art" src={album.image} alt={`The album art for ${album.title} by ${album.artists.join(', ')}.`}>
     <p class="title">{song.title}</p>
     <p class="artist">{song.artists.join(', ')}</p>
     <p class="album">{album.title}</p>
+    <p class="duration">{duration}</p>
+    <p class="plays">{song.plays}</p>
 </div>
 
 <style>
@@ -16,16 +26,28 @@
         display: flex;
         gap: 10px;
         align-items: center;
-        padding: 5px;
-        border: 2px solid var(--text);
     }
 
-    img {
+    .repetitions {
+        width: 30px;
+        display: flex;
+        justify-content: center;
+    }
+
+    .art {
         width: 50px;
         height: 50px;
     }
 
-    p {
-        width: 150px;
+    .title, .album {
+        width: 300px;
+    }
+
+    .artist {
+        width: 200px;
+    }
+
+    .duration, .plays {
+        width: 100px;
     }
 </style>
