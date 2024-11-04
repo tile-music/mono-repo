a3db3e44aacaa61f3a31fd85c8c324d394bdfe66
notsoli <samrandaofficial@gmail.com>
2024-11-03 22:13:40 -0500

    Fixed #30 and added ability to reset profile information

--- frontend/src/routes/(app)/profile/+page.server.ts
+++ frontend/src/routes/(app)/profile/+page.server.ts
@@ -3,13 +3,25 @@ import type { Actions } from './$types'
 
 export const load: PageServerLoad = async ({locals: { supabase, session } }) => {
     if (session !== null) {
-        const { data: user, error } = await supabase
+        // fetch profile data
+        let { data: user, error } = await supabase
         .from('profiles')
         .select(`updated_at, username, full_name, website, avatar_url`)
         .eq('id', session.user.id)
         .single()
-        if (error) console.error(error)
-        return { user: user ?? null }; 
+
+        if (error && error.code == "PGRST116") {
+            // insert new user profile
+            const blankProfile = assembleBlankProfile(session.user.id, session.user.email)
+            const { error: insertError } = await supabase.from('profiles').insert(blankProfile);
+            if (insertError) console.error(insertError)
+            else user = blankProfile
+        } else if (error) {
+            console.error(error);
+        }
+
+        // return the retrieved user, or the blank user if no profile was found
+        return { user: user ?? null, email: session.user.email ?? null }; 
     } else {
         throw Error("User does not have session.") 
     }
@@ -27,7 +39,7 @@ export const actions: Actions = {
                 username: formData.get('username') as string,
                 full_name: formData.get('full_name') as string,
                 website: formData.get('website') as string,
-                avatar_url: formData.get('avatar_url') as string,
+                avatar_url: null,
             }
       
             const { error } = await supabase.from('profiles').upsert(update)
@@ -35,5 +47,33 @@ export const actions: Actions = {
         } else {
             throw Error("User does not have session.") 
         }
+    },
+    reset_profile: async ({ request, locals: { supabase, session } }) => {
+        if (session !== null) {
+            const blankProfile = assembleBlankProfile(session.user.id, session.user.email);
+            const { error } = await supabase.from('profiles').upsert(blankProfile);
+            if (error) console.error(error);
+        } else {
+            throw Error("User does not have session.") 
+        }
     }
-  }
+}
+
+// TODO: TEST THISSSSS
+function assembleBlankProfile(id: string, email?: string) {
+    let username = null;
+    if (email) {
+        const domain = email.split('@')[0];
+        if (domain.length >= 3) username = domain;
+    }
+
+    // insert blank profile
+    return {
+        id: id,
+        updated_at: new Date(),
+        username,
+        full_name: null,
+        website: null,
+        avatar_url: null
+    }
+}
