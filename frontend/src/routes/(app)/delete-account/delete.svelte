a72034c6f030982ab9f985c4e34167687ed4a4df
notsoli <samrandaofficial@gmail.com>
2024-11-04 20:11:07 -0500

    Made profile deletion less verbose and added error handling

--- frontend/src/routes/(app)/delete-account/delete.svelte
+++ frontend/src/routes/(app)/delete-account/delete.svelte
@@ -4,43 +4,35 @@
   /**
    * Asynchronous function to delete an account.
    * 
-   * This function sends a POST request to the "/delete-account" endpoint with an empty JSON body.
-   * It logs the response status and throws an error if the response is not OK.
+   * This function sends a POST request (with the user's permission) to the "/delete-account" endpoint
+   * with an empty JSON body. It logs the response status and throws an error if the response is not OK.
    * 
    * @returns {Promise<Object>} The JSON response from the server if the request is successful.
    * @throws {Error} If the response status is not OK.
    */
-  let deleteAccount = async () =>
-    fetch("/delete-account", {
+  async function deleteAccount() {
+    // confirm with user
+    if (!confirm("Are you sure you want to delete your account? " +
+      "This action cannot be undone.")) return;
+    
+    const res = await fetch("/delete-account", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(" "),
-    })
-      .then((response) => {
-        console.log("Response status:", response.status); // Log status
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .catch((error) => console.error("Fetch error:", error));
+      body: null,
+    });
 
+    if (res.status == 200) {
+      goto('/logout');
+    } else {
+      const response = await res.json();
+      console.error(response.error);
+      goto('/profile');
+    }
+  }
 </script>
 
-<button
-  class="delete-account"
-  name="DeleteAccount"
-  on:click={async () => {
-    if (!confirm("Are you sure you want to delete your account? This action cannot be undone.")) {
-      return;
-    }
-    await deleteAccount()
-
-    await goto("/logout")
-  }}
-><slot /></button>
+<button class="delete-account" name="DeleteAccount" on:click={deleteAccount}>
+  <slot />
+</button>
 
 <style>
   button {
