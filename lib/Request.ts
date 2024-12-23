3f7350b421182699b1c304ca15ee4099298fcb43
notsoli <samrandaofficial@gmail.com>
2024-12-23 18:29:14 -0500

    Changed HTML date format to timestamps

--- lib/Request.ts
+++ lib/Request.ts
@@ -3,7 +3,7 @@ import type { SongInfo } from "./Song.ts";
 export type DisplayDataRequest = {
     aggregate: "song" | "album",
     num_cells: number | null,
-    date: { start: string | null, end: string | null},
+    date: { start: number | null, end: number | null},
     rank_determinant: "listens" | "time",
 };
 
