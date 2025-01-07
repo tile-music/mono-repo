9f2d79c00b3176a921f76163f6fc5bfd7b7bf52a
notsoli <samrandaofficial@gmail.com>
2025-01-06 19:44:41 -0500

    Added song info retrieval and ability to drag window

--- frontend/src/routes/(app)/display/Context.svelte
+++ frontend/src/routes/(app)/display/Context.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     import type { AlbumInfo } from "../../../../../lib/Song";
-    import type { DisplayDataRequest } from "../../../../../lib/Request";
+    import type { DisplayDataRequest, ContextDataRequest, ContextDataResponse } from "../../../../../lib/Request";
     import type { TimeFrame, DateStrings } from "./filters";
+    import { deserialize } from "$app/forms";
 
     // props
     export let album: AlbumInfo;
@@ -10,8 +11,6 @@
     export let dateStrings: DateStrings;
     export let filters: DisplayDataRequest;
     export let timeFrame: TimeFrame;
-
-    console.log(quantity);
 
     // computed state for metadata text
     $: artistsText = album.artists.length == 1 ? album.artists[0]
@@ -48,9 +47,62 @@
         const minutes = Math.floor((ms/1000/60/60 - hours)*60);
         return `${hours}:${minutes.toString().padStart(2, '0')}`;
     }
+
+    function toMinutesAndSeconds(ms: number) {
+        const date = new Date(ms);
+        return `${date.getMinutes()}:${date.getSeconds().toString().padStart(2, "0")}`;
+    }
+
+    // drag logic
+    let position = {top: 0, left: 0};
+    let moving = false;
+    let dragTarget: HTMLElement;
+
+    function onMouseDown(e: MouseEvent) {
+        if (e.target as HTMLElement == dragTarget) moving = true;
+    }
+
+    function onMouseUp() { moving = false; }
+
+    function onMouseMove(e: MouseEvent) {
+        if (moving) {
+            position.top += e.movementY;
+            position.left += e.movementX;
+        }
+    }
+
+    // form request logic
+    async function fetchContextMenuData(upc: string): Promise<ContextDataResponse> {
+        const request: ContextDataRequest = {
+            upc,
+            date: filters.date,
+            rank_determinant: filters.rank_determinant
+        }
+
+        const res = await fetch("?/context", {
+            method: "POST",
+            body: JSON.stringify(request),
+        });
+        const response = deserialize(await res.text());
+
+        if (response.type == "success") {
+            return response.data as ContextDataResponse;
+        } else if (response.type == "error") {
+            console.log(response.error);
+            throw response.error.message;
+        } else {
+            throw "Unknown error occurred.";
+        }
+    }
+
+    $: contextDataResponse = fetchContextMenuData(album.image);
 </script>
 
-<div id="context-menu">
+<div id="context-menu" style={`top:${position.top}px; left:${position.left}px`}>
+    <div id="dragTarget" bind:this={dragTarget} on:mousedown={onMouseDown}
+        role="button" tabindex={-1}>
+        <div id="handle"></div>
+    </div>
     <div id="metadata">
         <img src={album.image} alt={altText}>
         <div id="album-info">
@@ -60,17 +112,61 @@
             <p id="rank">{rankText} ({quantityText})</p>
         </div>
     </div>
+    <table id="tracklist">
+        <tr id="headers">
+            <th class="title"><h2>Tracklist</h2></th>
+            <th class="length"><h2>Length</h2></th>
+            <th class="plays"><h2>Plays</h2></th>
+        </tr>
+        {#await contextDataResponse}
+            <p>Waiting...</p>
+            {#each {length: album.tracks - 1} as _}
+                <tr>...</tr> <!-- placeholder songs to avoid jarring menu resize-->
+            {/each}
+        {:then response} 
+            {#each response.songs as entry}
+                <tr>
+                    <td class="title">{entry.song.title}</td>
+                    <td class="length">{toMinutesAndSeconds(entry.song.duration)}</td>
+                    <td class="plays">{filters.rank_determinant == "time" ?
+                        toHoursAndMinutes(entry.quantity) : entry.quantity}</td>
+                </tr>
+            {/each}
+            {#if album.tracks !== response.songs.length}
+                <p><em>{album.tracks - response.songs.length} have not been listened to yet.</em></p>
+            {/if}
+        {:catch error}
+            <p>Error: {error.toString()}</p>
+        {/await}
+    </table>
 </div>
+
+<svelte:window on:mouseup={onMouseUp} on:mousemove={onMouseMove} />
 
 <style>
     #context-menu {
         position: absolute;
-        top: 0;
-        left: 0;
-        width: min(500px, 100%);
-        height: min(600px, 100%);
-        padding: 10px;
+        width: min(400px, 100%);
+        height: min(800px, 100%);
+        max-height: fit-content;
+        padding: 5px 10px 10px 10px;
         background-color: var(--midground);
+        display: flex;
+        flex-direction: column;
+        overflow: scroll;
+    }
+
+    #dragTarget {
+        cursor: move;
+    }
+
+    #handle {
+        width: 100px;
+        height: 5px;
+        background-color: var(--medium);
+        margin: 0 auto 5px auto;
+        border-radius: 5px;
+        pointer-events: none;
     }
 
     #metadata {
@@ -92,5 +188,32 @@
 
     #artists {
         margin-bottom: 15px;
+    }
+
+    #tracklist {
+        width: 100%;
+        table-layout: fixed;
+        border-collapse: separate;
+        border-spacing: 0 10px;
+    }
+
+    th.title {
+        width: auto;
+    }
+
+    th.length {
+        width: 75px;
+    }
+
+    th.plays {
+        width: 75px;
+    }
+
+    .title {
+        text-align: left;
+    }
+
+    .length, .plays {
+        text-align: right;
     }
 </style>
