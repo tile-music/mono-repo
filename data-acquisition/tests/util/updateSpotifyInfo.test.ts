93d02c5aad213294bb578d9ffe0ac3d50d13c704
Ivy Bixler <ivybixler@gmail.com>
2025-05-23 14:36:31 -0400

    tests now work

--- data-acquisition/tests/util/updateSpotifyInfo.test.ts
+++ data-acquisition/tests/util/updateSpotifyInfo.test.ts
@@ -1,36 +1,32 @@
-
+import { SupabaseClient } from "jsr:@supabase/supabase-js@2";
 import { SpotifyUpdateData, updateSpotifyAlbumPopularityHelper, selectString, updateSpotifyAlbumPopularity } from "../../src/util/updateSpotifyInfo.ts";
-
+import { Client } from "npm:spotify-api.js@latest"
 import { SpotifyUserPlaying } from "../../src/music/UserPlaying.ts";
+
 import { expect } from "jsr:@std/expect";
+let supabase: SupabaseClient<any, "test", any> = new SupabaseClient(
+  Deno.env.get("SB_URL_TEST") as string,
+  Deno.env.get("SERVICE") as string,
+  { db: { schema: "test" } }
+);;
 
+let spotifyData: SpotifyUpdateData[] = [];
 
+const spotifyClient: Client = await Client.create({
+  refreshToken: true,
+  token: {
+    clientID: Deno.env.get("SP_CID") as string,
+    clientSecret: Deno.env.get("SP_SECRET") as string,
+    refreshToken: Deno.env.get("SP_REFRESH") as string,
+  },
+  onRefresh: () => {
+    console.log("token refreshed");
+  },
+});
 
+Deno.test({ name : "Test updateSpotifyAlbumPopularity", async  fn (t: Deno.TestContext) {
 
-
-Deno.test("Test updateSpotifyAlbumPopularity", async (t: Deno.TestContext) => {
-  console.log("dfsf")
-  let supabase: SupabaseClient = new SupabaseClient(
-    Deno.env.get("SB_URL_TEST") || "",
-    Deno.env.get("SERVICE") || "",
-    
-  );
-  let spotifyClient: Client;
-  let spotifyData: SpotifyUpdateData[] = [];
-
-  spotifyClient = await Client.create({
-    refreshToken: true,
-    token: {
-      clientID: Deno.env.get("SP_CID") || "",
-      clientSecret: Deno.env.get("SP_SECRET") || "",
-      refreshToken: Deno.env.get("SP_REFRESH") || "",
-    },
-    onRefresh: () => {
-      console.log("token refreshed");
-    },
-  });
-
-  //you can use this if you need to remake the test data
+  //you can use this if you need to remake the test data you will also need to convert it to deno
   /* describe("fetch testdata from db", () => {
     test("get data ", async () => {
       const userPlayedData = await supabase.schema("prod").from("played_tracks").select(selectString).limit(25);
@@ -39,10 +35,32 @@ Deno.test("Test updateSpotifyAlbumPopularity", async (t: Deno.TestContext) => {
     });
   }) */
 
-  await t.step("test update SpotifyAlbumPopularityHelper", async (t: Deno.TestContext) => {
-    const data = fs.readFileSync('tests/util/test-data.json', 'utf8');
+  const { data, error } = await supabase.auth.signUp({
+    email: "test2@example.com",
+    password: "password",
+  });
+  if (error) throw error;
+  const userId = data.user?.id as string;
+  const spotifyUserPlaying = new SpotifyUserPlaying(
+    supabase,
+    userId,
+    { refresh_token: Deno.env.get("SP_REFRESH") }
+  );
+  /** the type should be some kind of postgrest builder but im lazy👅 */
+
+  if (userId === '' || userId === undefined) throw Error("userId is empty");
+
+  await spotifyUserPlaying.init();
+  await spotifyUserPlaying.fire();
+
+  const query = (supabase.schema("test").from("played_tracks").select(selectString).eq("user_id", userId))
+
+
+  await t.step("test update SpotifyAlbumPopularityHelper", async () => {
+    const dataArr = await Deno.readFile('tests/util/test-data.json');
+    const data = new TextDecoder().decode(dataArr);
     spotifyData = JSON.parse(data);
-    console.log("spotifyData: ", spotifyData.length);
+
     const ret = await updateSpotifyAlbumPopularityHelper(spotifyClient.token, "test", false, undefined, spotifyData)
     expect(ret.size).toBe(spotifyData.length);
     ret.forEach((r) => {
@@ -50,35 +68,11 @@ Deno.test("Test updateSpotifyAlbumPopularity", async (t: Deno.TestContext) => {
       expect(r.album_popularity).toBeGreaterThanOrEqual(0);
     })
   });
-  t.step("test using real data", async (t) => {
-    let userId: string;
-    let spotifyUserPlaying;
-    /** the type should be some kind of postgrest builder but im lazy👅 */
-    let query;
-
-    const { data, error } = await supabase.auth.signUp({
-      email: "test4@example.com",
-      password: "password",
-    });
-    if (error) throw error;
-    userId = data.user?.id || "";
-    if (userId === '' || userId === undefined) throw Error("userId is empty");
-    spotifyUserPlaying = new SpotifyUserPlaying(
-      supabase,
-      userId,
-      { refresh_token: Deno.env.get("SP_REFRESH") }
-    );
-    await spotifyUserPlaying.init();
-    await spotifyUserPlaying.fire();
-    query = (supabase.schema("test").from("played_tracks").select(selectString).eq("user_id", userId))
-
-    afterAll(async () => {
-      await supabase.auth.admin.deleteUser(userId);
-    })
-    t.step("use real data", async () => {
+  await t.step("test using real data", async (t) => {
+    await t.step("use real data", async () => {
       await updateSpotifyAlbumPopularityHelper(spotifyClient.token, "test", false)
-      let { data, error } = await query;
-      let typedData = data as unknown as SpotifyUpdateData[];
+      const { data, error } = await query;
+      const typedData = data as unknown as SpotifyUpdateData[];
       if (error) throw error;
       expect(data?.length).toBeGreaterThan(0);
       typedData?.forEach((d) => {
@@ -86,7 +80,7 @@ Deno.test("Test updateSpotifyAlbumPopularity", async (t: Deno.TestContext) => {
         expect(d.album_popularity === null).toBeFalsy();
       })
     })
-    t.step("test update using real data with albums having no spotify ids", async () => {
+    await t.step("test update using real data with albums having no spotify ids", async () => {
       let localQuery = query;
       localQuery = localQuery.gte("listened_at", new Date(Date.now().valueOf() - (1000 * 60 * 60 * 24)).valueOf());
       await supabase.schema("test").from("played_tracks").update({ album_popularity: null }).eq("user_id", userId)
@@ -104,4 +98,5 @@ Deno.test("Test updateSpotifyAlbumPopularity", async (t: Deno.TestContext) => {
       })
     })
   });
-});
+  await supabase.auth.admin.deleteUser(userId);
+}, sanitizeOps: false, sanitizeResources: false, sanitizeExit: false });
