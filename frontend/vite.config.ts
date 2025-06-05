fe3af1902dbc76e9df9b24e40de3290f52c12c96
samranda <samrandaofficial@gmail.com>
2025-06-05 16:03:36 -0400

    Fixed file uploading and serving

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,11 @@ export default defineConfig({
     server: {
         proxy: {
             "/location": "https://mdv-test-h0019375.cs.wpi.edu/",
+            "/assets": {
+                target: 'http://kong:8000/storage/v1/object',
+                changeOrigin: true,
+                rewrite: path => path.replace(/^\/assets/, ""),
+            }
         },
         watch: {
             usePolling: true,
