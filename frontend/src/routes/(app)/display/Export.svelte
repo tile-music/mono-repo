841f42517164152b3d2db6241888d92f599d45a9
samranda <samrandaofficial@gmail.com>
2025-06-16 23:32:26 -0400

    Added profile image in export header and orientation options

--- frontend/src/routes/(app)/display/Export.svelte
+++ frontend/src/routes/(app)/display/Export.svelte
@@ -12,8 +12,10 @@
 
     let canvas: HTMLCanvasElement;
     let exportImage: HTMLImageElement | null = $state(null);
-    let status: "idle" | "exporting" | "error" | "done" = $state("idle");
+    let status: "idle" | "configuring" | "exporting" | "error" | "done" = $state("idle");
     let exportError: string | null = $state(null);
+    let orientation: "portrait" | "landscape" | "square" = $state("portrait");
+    $inspect(orientation);
 
     function handleExportError(error: string) {
         console.error(error);
@@ -22,11 +24,21 @@
     }
 
     // Poster and layout config
-    const POSTER_WIDTH = 18 * 300;
-    const POSTER_HEIGHT = 24 * 300;
+    let posterWidth = $derived.by(() => {
+        switch (orientation) {
+            case "portrait": return 18 * 300;
+            case "landscape": return 24 * 300;
+            case "square": return 20 * 300;
+        }
+    });
+    let posterHeight = $derived.by(() => {
+        switch (orientation) {
+            case "portrait": return 24 * 300;
+            case "landscape": return 18 * 300;
+            case "square": return 20 * 300;
+        }
+    });
     const MARGIN = 200;
-    const HEADER_HEIGHT = 500;
-    const FOOTER_HEIGHT = 200;
     const IMAGE_MARGIN = 30;
     const HEADER_FONT_SIZE = 200;
     const FOOTER_FONT_SIZE = 100;
@@ -52,8 +64,8 @@
         }
 
         // prepare canvas
-        canvas.width = POSTER_WIDTH;
-        canvas.height = POSTER_HEIGHT;
+        canvas.width = posterWidth;
+        canvas.height = posterHeight;
         context.fillStyle = "black";
         context.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -81,17 +93,52 @@
         }
 
         // draw header
+        let headerHeight = MARGIN;
         if (generalOptions.headerOptions.showHeader) {
             if (!profile) {
                 handleExportError("profile is not defined");
                 return;
             }
 
+            headerHeight += HEADER_FONT_SIZE + MARGIN;
+            let headerTextOffset = MARGIN;
+
+            // draw profile image with circle mask
+            if (generalOptions.headerOptions.showAvatar) {
+                headerHeight += HEADER_FONT_SIZE * 0.5;
+                const radius = HEADER_FONT_SIZE * 0.75;
+
+                const profileImage = new Image();
+                profileImage.crossOrigin = "anonymous";
+                profileImage.src = profile.avatar_url;
+
+                await new Promise<void>((resolve, reject) => {
+                    profileImage.onload = () => {
+                        headerTextOffset += radius * 2 + MARGIN / 2;
+                        context.save();
+                        context.beginPath();
+                        context.arc(
+                            MARGIN + radius, MARGIN + radius,
+                            radius, 0, Math.PI * 2
+                        );
+                        context.clip();
+                        context.drawImage(
+                            profileImage,
+                            MARGIN, MARGIN, radius * 2, radius * 2
+                        );
+                        context.restore();
+                        resolve();
+                    };
+                    profileImage.onerror = () => reject("failed to load profile image");
+                });
+            }
+
+
             context.fillStyle = "white";
             context.font = `${HEADER_FONT_SIZE}px Mattone`;
             context.fillText(
                 getHeadingText(profile, generalOptions.headerOptions, filters, filtersContext),
-                MARGIN, MARGIN + HEADER_FONT_SIZE
+                headerTextOffset, headerHeight / 2 + HEADER_FONT_SIZE / 2
             );
         }
 
@@ -100,7 +147,8 @@
             const square = arrangement.squares.list[i];
 
             const squareCoords = squareToPosterCoords(
-                square, arrangement.squares.width, arrangement.squares.height
+                square, arrangement.squares.width, arrangement.squares.height,
+                headerHeight
             );
 
             context.drawImage(
@@ -116,7 +164,7 @@
         context.textAlign = "right";
         context.fillText(
             `tile.music`,
-            POSTER_WIDTH - MARGIN, POSTER_HEIGHT - MARGIN
+            posterWidth - MARGIN, posterHeight - MARGIN
         );
 
         // display the rendered canvas as an image
@@ -126,17 +174,21 @@
         }
 
         exportImage.src = canvas.toDataURL("image/jpeg");
+        exportImage.width = posterWidth / 16;
+        exportImage.height = posterHeight / 16;
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         status = "done";
     }
 
     function squareToPosterCoords(
-        square: SquareInfo, arrangementWidth: number, arrangementHeight: number
+        square: SquareInfo, arrangementWidth: number, arrangementHeight: number,
+        headerHeight: number
     ) {
         // compute available drawing area
-        const drawWidth = POSTER_WIDTH - 2 * MARGIN;
-        const drawHeight = POSTER_HEIGHT - HEADER_HEIGHT - FOOTER_HEIGHT - 2 * MARGIN;
+        const footerHeight = FOOTER_FONT_SIZE + MARGIN * 2;
+        const drawWidth = posterWidth - 2 * MARGIN;
+        const drawHeight = posterHeight - headerHeight - footerHeight;
 
         // arrangement aspect ratio
         const arrangementAspect = arrangementWidth / arrangementHeight;
@@ -148,12 +200,12 @@
             // arrangement is wider than drawing area
             scale = drawWidth / arrangementWidth;
             offsetX = MARGIN;
-            offsetY = HEADER_HEIGHT + MARGIN + (drawHeight - arrangementHeight * scale) / 2;
+            offsetY = headerHeight + (drawHeight - arrangementHeight * scale) / 2;
         } else {
             // arrangement is taller than drawing area
             scale = drawHeight / arrangementHeight;
             offsetX = MARGIN + (drawWidth - arrangementWidth * scale) / 2;
-            offsetY = HEADER_HEIGHT + MARGIN;
+            offsetY = headerHeight
         }
 
         return {
@@ -166,14 +218,30 @@
 
 <button
     class="art-display-button"
-    onclick={exportCanvas}
+    onclick={() => { status = "configuring"; }}
     disabled={status !== "idle"}
 >export</button>
 {#if status !== "idle"}
     <div id="popup-container">
         <div id="popup">
             <h1>export display</h1>
-            {#if status === "exporting"}
+            {#if status === "configuring"}
+                <div class="input-group">
+                    <label for="orientation">orientation</label>
+                    <select
+                        name="orientation" id="orientation"
+                        bind:value={orientation}
+                    >
+                        <option value="portrait">portrait</option>
+                        <option value="landscape">landscape</option>
+                        <option value="square">square</option>
+                    </select>
+                </div>
+                <button
+                    class="art-display-button"
+                    onclick={exportCanvas}
+                >export</button>
+            {:else if status === "exporting"}
                 <p>exporting...</p>
             {:else if status === "error"}
                 <p class="error">error: {exportError}</p>
@@ -182,7 +250,7 @@
             {/if}
             <img bind:this={exportImage} hidden={status !== "done"} src="" alt="">
             <button
-                class="art-display-button"
+                id="close"
                 onclick={() => {
                     status = "idle";
                     exportError = null;
@@ -217,12 +285,31 @@
         margin: auto 0;
     }
 
-    img {
-        width: 360px;
-        height: 480px;
-    }
-
     canvas {
         display: none;
+    }
+
+    #close {
+        color: var(--accent);
+        text-decoration: underline;
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+    }
+
+    select {
+        background-color: var(--midground);
+        border: 0;
+        border-bottom: 2px solid var(--medium);
+        font-family: "Archivo", sans-serif;
+        font-size: 15px;
+        padding:  2px; /* compensate for border */
+        color: var(--text);
+    }
+
+    .input-group {
+        display: flex;
+        gap: 20px;
+        align-items: center;
     }
 </style>
