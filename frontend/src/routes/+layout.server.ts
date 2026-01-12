d035be2c379ef5769f90e22232ba2d12a9307464
samranda <samrandaofficial@gmail.com>
2026-01-12 17:36:24 -0500

    Remove `session` and `cookies` from PageData to reflect actual type and usage

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,13 +1,5 @@
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = async ({
-    locals: { safeGetSession, profile },
-    cookies,
-}) => {
-    const { session } = await safeGetSession();
-    return {
-        session,
-        cookies: cookies.getAll(),
-        profile
-    };
+export const load: LayoutServerLoad = async ({ locals: { profile } }) => {
+    return { profile };
 };
