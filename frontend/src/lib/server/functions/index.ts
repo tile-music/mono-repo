42b64aba5cbdf5566c371b961fbb1517400c9ea8
samranda <samrandaofficial@gmail.com>
2026-01-12 21:06:24 -0500

    Relocated get-listening-data to frontend

--- frontend/src/lib/server/functions/index.ts
+++ frontend/src/lib/server/functions/index.ts
@@ -1,5 +1,6 @@
 import checkSpotifyLogin from "./checkSpotifyLogin";
 import getContextData from "./getContextData";
 import getDisplayData from "./getDisplayData";
+import getListeningData from "./getListeningData";
 
-export { checkSpotifyLogin, getContextData, getDisplayData };
+export { checkSpotifyLogin, getContextData, getDisplayData, getListeningData };
